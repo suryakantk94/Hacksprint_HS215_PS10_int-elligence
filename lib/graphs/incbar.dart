1186a8820d895c36202ec9a17c7c5f740de92c95
Areeba Shaikh <shaikhareeba22@gmail.com>
2020-09-26 10:03:58 +0530

    Show live chart

--- lib/graphs/incbar.dart
+++ lib/graphs/incbar.dart
@@ -3,14 +3,18 @@ import 'package:pockett/animation/FadeAnimation.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:pockett/graphs/barchart.dart';
 import 'package:pockett/graphs/barchart1.dart';
-
+import '../handlers/database-manager.dart';
+import '../entities/DateExpense.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:intl/intl.dart';
 class Expensegraph extends StatefulWidget {
   @override
   _ExpensegraphState createState() => _ExpensegraphState();
 }
 
 class _ExpensegraphState extends State<Expensegraph> {
-  final List<IncomeAmt> data = [
+  final List<IncomeAmt> staticData = [
     IncomeAmt(
       date: "30 June",
       amt: 20,
@@ -49,55 +53,102 @@ class _ExpensegraphState extends State<Expensegraph> {
   ];
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-          Colors.blue[900],
-          Colors.blue[800],
-          Colors.blue[400]
-        ])),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: SingleChildScrollView(
-                    child: FadeAnimation(
-                        1.4,
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Color.fromRGBO(20, 17, 237, .4),
-                                    blurRadius: 20,
-                                    offset: Offset(0, 10))
-                              ]),
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                    height: 350,
-                                    width: 600,
-                                    child: BarChart1(data: data)),
-                              ]),
-                        )),
-                  ),
+  void initState() {
+    super.initState();
+    timer =
+    new Timer.periodic(new Duration(seconds: 20), (t) => getIncomeData());
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  List data;
+  Timer timer;
+  handleGetDataSuccess(String response) {
+    setState(() {
+      data = jsonDecode(response);
+    });
+  }
+
+  getIncomeData() async {
+    Future<dynamic> loginFuture = DBManager.db.getExpenses();
+    loginFuture
+        .then((data) => {handleGetDataSuccess(data)})
+        .catchError((err) => {print("Error getting data")});
+  }
+  createChart() {
+    var newFormat = DateFormat("dd MMM");
+    print(data);
+    //List<DateExpense> apiData = List<DateExpense>.from(data).map((Map model) => DateExpense.fromJson(model)).toList();
+    List<DateExpense> apiData =
+    data.map((dJson) => DateExpense.fromJson(dJson)).toList();
+    print(apiData);
+    List<IncomeAmt> modifiedData = apiData
+        .map((s) => new IncomeAmt(
+        date: newFormat.format(s.date),
+        amt: s.total.round(),
+        barColor: charts.ColorUtil.fromDartColor(Colors.blue[500])))
+        .toList();
+    print("\n\n\n\t\t\t");
+    print(modifiedData);
+    //l.add(12);
+
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+            Colors.blue[900],
+            Colors.blue[800],
+            Colors.blue[400]
+          ])),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: SingleChildScrollView(
+                  child: FadeAnimation(
+                      1.4,
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color.fromRGBO(20, 17, 237, .4),
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10))
+                            ]),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                  height: 350,
+                                  width: 600,
+                                  child: BarChart1(data: modifiedData)),
+                            ]),
+                      )),
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: data == null ? CircularProgressIndicator() : createChart(),
     );
   }
 }
--- lib/handlers/database-manager.dart
+++ lib/handlers/database-manager.dart
@@ -8,19 +8,16 @@ import '../entities/User.dart';
 import '../entities/Expense.dart';
 import '../entities/DateExpense.dart';
 
+const baseUrl = "https://pockett-215.herokuapp.com";
+
+// https://pure-reaches-26058.herokuapp.com
 class DBManager {
   DBManager._();
 
-  String baseUrl = "https://pure-reaches-26058.herokuapp.com";
-  String baseUsersUrl = "https://pure-reaches-26058.herokuapp.com/users";
-  String baseExpensesUrl = "https://pure-reaches-26058.herokuapp.com/expenses";
-  String baseLoginUrl = "https://pure-reaches-26058.herokuapp.com/login";
+  String baseUsersUrl = baseUrl + "/users";
+  String baseExpensesUrl = baseUrl + "/expenses";
+  String baseLoginUrl = baseUrl + "/login";
 
-/*
-  String baseUrl = "http://localhost:3000";
-  String baseUsersUrl = "http://localhost:3000/users";
-  String baseLoginUrl = "http://localhost:3000/auth";
-*/
   static final DBManager db = DBManager._();
 
   dynamic _response(http.Response response) {
@@ -99,7 +96,8 @@ class DBManager {
       print("oldLimit: " + newLimit.toString());
 
       if (newLimit != oldLimit) {
-        return "You have exhausted daily expense quota. To keep your expenses in control, your daily limit has been adjusted to INR " + newLimit.toString();
+        return "You have exhausted daily expense quota. To keep your expenses in control, your daily limit has been adjusted to INR " +
+            newLimit.toString();
       } else {
         return "";
       }
@@ -143,38 +141,42 @@ class DBManager {
           }));
 
       responseJson = _response(response);
-
     } on SocketException {
       throw FetchDataException('No Internet connection');
     }
     return responseJson;
   }
 
-  Future<dynamic> getBalance() async {
+  Future<dynamic> getExpenses() async {
     print("Getting balance");
     String url = baseExpensesUrl + "/" + Globals.loggedInUser.id;
 
     var responseJson;
     try {
-      final response = await http.get(url,
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8',
-          });
+      final response = await http.get(url, headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      });
 
       responseJson = _response(response);
-      print(responseJson);
-      var objsJson = jsonDecode(responseJson) as List;
-      print(objsJson);
-      List<DateExpense> expObjs = objsJson.map((dJson) => DateExpense.fromJson(dJson)).toList();
-      print(expObjs);
-      double totalExpense = expObjs.fold(0, (sum, item) => sum + item.total);
-      print("total is: " + totalExpense.toString());
-      Globals.balance = Globals.loggedInUser.monthlyIncome - totalExpense;
-      print(Globals.balance);
     } on SocketException {
       throw FetchDataException('No Internet connection');
     }
     return responseJson;
+  }
+
+  handleGetBalanceSuccess(String responseJson) {
+    var objsJson = jsonDecode(responseJson) as List;
+    List<DateExpense> expObjs =
+        objsJson.map((dJson) => DateExpense.fromJson(dJson)).toList();
+    double totalExpense = expObjs.fold(0, (sum, item) => sum + item.total);
+    Globals.balance = Globals.loggedInUser.monthlyIncome - totalExpense;
+  }
+
+  Future<dynamic> getBalance() async {
+    print("Getting balance");
+    getExpenses()
+        .then((responseJson) => {handleGetBalanceSuccess(responseJson)})
+        .catchError((err) => {print("Error getting expenses")});
   }
 
   Future<void> setMonthlyIncome(double i) async {
@@ -236,6 +238,7 @@ class DBManager {
     }
     return responseJson;
   }
+
   addUsernameToSF(String s) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString('Username', s);
